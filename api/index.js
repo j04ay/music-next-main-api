028da8a09f130d5a96c9b473a51f13beaae41f54
j04ay <1272765934@qq.com>
2026-03-26 03:03:14 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 
-// 解决 Vercel 跨域
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+// ******** 解决跨域（必须加！Vercel 强制要求） ********
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
   next()
 })
 
+// ******** 注册你的代理路由（不变） ********
 const registerRouter = require('../router')
 registerRouter(app)
 
