36f877cf775707b134dc8f234b92fcb6dc65f533
j04ay <1272765934@qq.com>
2026-03-26 02:02:21 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
-const registerRouter = require('../registerRouter.js');
+// 解决跨域
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
+
+const registerRouter = require('../router');
 registerRouter(app);
 
 module.exports = app;
