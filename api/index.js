db11a569d3f2d9900e773619957c6aa97a726c81
j04ay <1272765934@qq.com>
2026-03-26 02:04:08 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 
-// 解决跨域
-app.all('*', function (req, res, next) {
+// -------------- 跨域配置（必须放在最前面！）--------------
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
+// 你的路由
 const registerRouter = require('../router');
 registerRouter(app);
 
