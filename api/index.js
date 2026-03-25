a65cfb01c0686b471a42e145fed1ea998adb8cd7
j04ay <1272765934@qq.com>
2026-03-26 02:10:54 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
+// ======================================
 // 跨域
+// ======================================
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  if (req.method === 'OPTIONS') {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Max-Age", "86400");
+  
+  if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
   next();
 });
 
+// 路由
 const registerRouter = require('../router');
 registerRouter(app);
 
